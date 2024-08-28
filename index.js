3a8979898663abc2b1df55f04856730f0729e1fc
thanhhiuu <thanhhieu961906@gmail.com>
2024-08-28 17:53:01 +0700

    o

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+// Cấu hình các nguồn được phép
+const allowedOrigins = [
+  process.env.VITE_NODE_ENV,
+  'https://zingmp3api-qwgj.onrender.com',
+  'https://project-song.vercel.app',
+];
+
+// Middleware CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 // Page Home
 app.get('/', (req, res) => {
   res.send('SERVER ON');
@@ -12,14 +30,7 @@ app.get('/', (req, res) => {
 
 // ZingMp3Router
 const ZingMp3Router = require('./src/routes/ZingRouter');
-app.use(
-  '/api',
-  cors({
-    origin: process.env.VITE_NODE_ENV,
-    origin: 'https://project-song.vercel.app',
-  }),
-  ZingMp3Router
-);
+app.use('/api', cors(corsOptions), ZingMp3Router);
 
 // Page Error
 app.get('*', (req, res) => {
